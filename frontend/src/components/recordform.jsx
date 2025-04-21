893e685b76aa84e02ce5eae012b5af433af4461e
Jeevith K <104653693+JeevithK@users.noreply.github.com>
2025-04-21 19:06:02 +0530

    Update recordform.jsx

--- frontend/src/components/recordform.jsx
+++ frontend/src/components/recordform.jsx
@@ -30,7 +30,7 @@ const Recordform = ({ fetchRecords }) => {
         setFormData((prev) => ({ ...prev, date: formatteddate }));
 
         const postrecord = await axios.post(
-        "https://finance-tracker-by-jk1.onrender.com",
+        "https://finance-tracker-by-jk1.onrender.com/createrecord",
         formData
         );
 
